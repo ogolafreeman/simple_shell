2296b017be9949734d708fdc4fb752af2ade71bd
Reeny12 <lebotsamaureen@gmail.com>
2023-05-26 12:48:04 -0700

    Functions

--- lists_code_1.c
+++ lists_code_1.c
@@ -1,12 +1,11 @@
 #include "shell.h"
 
 /**
- * add_node - adds a node to the start of the list
- * @head: address of pointer to head node
- * @str: str field of node
- * @num: node index used by history
- *
- * Return: size of list
+ * add_node - ahju
+ * @head: ahju
+ * @str: ahju
+ * @num: ahju
+ * Return: ahju
  */
 list_t *add_node(list_t **head, const char *str, int num)
 {
@@ -34,12 +33,11 @@ list_t *add_node(list_t **head, const char *str, int num)
 }
 
 /**
- * add_node_end - adds a node to the end of the list
- * @head: address of pointer to head node
- * @str: str field of node
- * @num: node index used by history
- *
- * Return: size of list
+ * add_node_end - ahju
+ * @head: ahju
+ * @str: ahju
+ * @num: ahju
+ * Return: ahju
  */
 list_t *add_node_end(list_t **head, const char *str, int num)
 {
@@ -75,10 +73,9 @@ list_t *add_node_end(list_t **head, const char *str, int num)
 }
 
 /**
- * print_list_str - prints only the str element of a list_t linked list
- * @h: pointer to first node
- *
- * Return: size of list
+ * print_list_str - ahju
+ * @h: ahju
+ * Return: ahju
  */
 size_t print_list_str(const list_t *h)
 {
@@ -95,11 +92,10 @@ size_t print_list_str(const list_t *h)
 }
 
 /**
- * delete_node_at_index - deletes node at given index
- * @head: address of pointer to first node
- * @index: index of node to delete
- *
- * Return: 1 on success, 0 on failure
+ * delete_node_at_index - ahju
+ * @head: ahju
+ * @index: ahju
+ * Return: ahju
  */
 int delete_node_at_index(list_t **head, unsigned int index)
 {
@@ -135,10 +131,9 @@ int delete_node_at_index(list_t **head, unsigned int index)
 }
 
 /**
- * free_list - frees all nodes of a list
- * @head_ptr: address of pointer to head node
- *
- * Return: void
+ * free_list - ahju
+ * @head_ptr: ahju
+ * Return: ahju
  */
 void free_list(list_t **head_ptr)
 {
--- lists_code_2.c
+++ lists_code_2.c
@@ -1,10 +1,9 @@
 #include "shell.h"
 
 /**
- * list_len - determines length of linked list
- * @h: pointer to first node
- *
- * Return: size of list
+ * list_len - ahju
+ * @h: ahju
+ * Return: ahju
  */
 size_t list_len(const list_t *h)
 {
@@ -19,10 +18,9 @@ size_t list_len(const list_t *h)
 }
 
 /**
- * list_to_strings - returns an array of strings of the list->str
- * @head: pointer to first node
- *
- * Return: array of strings
+ * list_to_strings - ahju
+ * @head: ahju
+ * Return: ahju
  */
 char **list_to_strings(list_t *head)
 {
@@ -56,10 +54,9 @@ char **list_to_strings(list_t *head)
 
 
 /**
- * print_list - prints all elements of a list_t linked list
- * @h: pointer to first node
- *
- * Return: size of list
+ * print_list - ahju
+ * @h: ahju
+ * Return: ahju
  */
 size_t print_list(const list_t *h)
 {
@@ -79,12 +76,11 @@ size_t print_list(const list_t *h)
 }
 
 /**
- * node_starts_with - returns node whose string starts with prefix
- * @node: pointer to list head
- * @prefix: string to match
- * @c: the next character after prefix to match
- *
- * Return: match node or null
+ * node_starts_with - ahju
+ * @node: ahju
+ * @prefix: ahju
+ * @c: ahju
+ * Return: ahju
  */
 list_t *node_starts_with(list_t *node, char *prefix, char c)
 {
@@ -101,11 +97,10 @@ list_t *node_starts_with(list_t *node, char *prefix, char c)
 }
 
 /**
- * get_node_index - gets the index of a node
- * @head: pointer to list head
- * @node: pointer to the node
- *
- * Return: index of node or -1
+ * get_node_index - ahju
+ * @head: ahju
+ * @node: ahju
+ * Return: ahju
  */
 ssize_t get_node_index(list_t *head, list_t *node)
 {
