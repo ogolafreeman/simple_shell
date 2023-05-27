ead1e9fc9eb8566b5cd3732659fa663e42f66225
Reeny12 <lebotsamaureen@gmail.com>
2023-05-26 22:30:15 -0700

    Functions

--- realloc.c
+++ realloc.c
@@ -11,6 +11,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
+	/*for loop*/
 	for (i = 0; i < n; i++)
 		s[i] = b;
 	return (s);
@@ -24,8 +25,11 @@ void ffree(char **pp)
 {
 	char **a = pp;
 
+	/*if statement*/
 	if (!pp)
+		/*breaks if pointer is null*/
 		return;
+	/*while loop*/
 	while (*pp)
 		free(*pp++);
 	free(a);
@@ -42,18 +46,23 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *p;
 
+	/*if statement*/
 	if (!ptr)
 		return (malloc(new_size));
+	/*if statement*/
 	if (!new_size)
 		return (free(ptr), NULL);
+	/*if statement*/
 	if (new_size == old_size)
 		return (ptr);
 
 	p = malloc(new_size);
+	/*if statement*/
 	if (!p)
 		return (NULL);
 
 	old_size = old_size < new_size ? old_size : new_size;
+	/*while loop*/
 	while (old_size--)
 		p[old_size] = ((char *)ptr)[old_size];
 	free(ptr);
