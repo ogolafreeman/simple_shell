cbf1825a702bf4c70fdbfd619bfa6f39d04709cf
Reeny12 <lebotsamaureen@gmail.com>
2023-05-26 22:23:14 -0700

    Functions

--- tokenizer.c
+++ tokenizer.c
@@ -12,21 +12,26 @@ char **strtow(char *str, char *d)
 	int i, j, k, m, numwords = 0;
 	char **s;
 
+	/*if statement*/
 	if (str == NULL || str[0] == 0)
 		return (NULL);
+	/*if statement*/
 	if (!d)
 		d = " ";
+	/*if statement*/
 	for (i = 0; str[i] != '\0'; i++)
 		if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
 			numwords++;
-
+	/*if statement*/
 	if (numwords == 0)
 		return (NULL);
 	s = malloc((1 + numwords) * sizeof(char *));
 	if (!s)
 		return (NULL);
+	/*for loop*/
 	for (i = 0, j = 0; j < numwords; j++)
 	{
+		/*while loop*/
 		while (is_delim(str[i], d))
 			i++;
 		k = 0;
@@ -59,25 +64,32 @@ char **strtow2(char *str, char d)
 	int i, j, k, m, numwords = 0;
 	char **s;
 
+	/*if statement*/
 	if (str == NULL || str[0] == 0)
 		return (NULL);
+	/*if statement*/
 	for (i = 0; str[i] != '\0'; i++)
 		if ((str[i] != d && str[i + 1] == d) ||
 		    (str[i] != d && !str[i + 1]) || str[i + 1] == d)
 			numwords++;
+	/*if statement*/
 	if (numwords == 0)
 		return (NULL);
 	s = malloc((1 + numwords) * sizeof(char *));
 	if (!s)
 		return (NULL);
+	/*if statement*/
 	for (i = 0, j = 0; j < numwords; j++)
 	{
+		/*if statement*/
 		while (str[i] == d && str[i] != d)
 			i++;
 		k = 0;
+		/*if statement*/
 		while (str[i + k] != d && str[i + k] && str[i + k] != d)
 			k++;
 		s[j] = malloc((k + 1) * sizeof(char));
+		/*if statement*/
 		if (!s[j])
 		{
 			for (k = 0; k < j; k++)
